8a56a505b84fa10be17d879b70c381b6356bac69
lbjworld <lbj.world@gmail.com>
2017-09-22 15:34:10 +0800

    FIX: setup

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from distutils.core import setup
 setup(name='gym_trading',
       version='0.0.2',
       install_requires=['gym', 'pandas', 'pandas_datareader'],
-      packages=['gym_trading', 'gym_trading.envs'],
+      packages=['gym_trading'],
+      package_dir={'gym_trading': ''},
 )
