64f19730f3444def582c639571eb824f742c54a6
lbjworld <lbj.world@gmail.com>
2017-09-22 15:19:29 +0800

    FIX: setup

--- setup.py
+++ setup.py
@@ -4,5 +4,4 @@ setup(name='gym_trading',
       version='0.0.2',
       install_requires=['gym', 'pandas', 'pandas_datareader'],
       packages=['gym_trading'],
-      package_dir={'gym_trading': 'gym_trading'},
 )
