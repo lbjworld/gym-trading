9ef75ec1bb01a821b41b1d7161915361b5225eb4
lbjworld <lbj.world@gmail.com>
2017-09-22 14:02:19 +0800

    FIX: enable market env

--- gym_trading/__init__.py
+++ gym_trading/__init__.py
@@ -5,8 +5,8 @@ register(
     entry_point='gym_trading.envs:TradingEnv',
     timestep_limit=1000,
 )
-#register(
-#    id='foo-extrahard-v0',
-#    entry_point='gym_foo.envs:FooExtraHardEnv',
-#    timestep_limit=1000,
-#)
+register(
+    id='complex-trading-v0',
+    entry_point='gym_trading.envs:MultiStockTradingEnv',
+    timestep_limit=1000,
+)
