7ef3f9b4b5c7a785060889361eec47ed79409e8a
lbjworld <lbj.world@gmail.com>
2017-11-11 13:55:02 +0800

    FIX: init

--- gym_trading/__init__.py
+++ gym_trading/__init__.py
@@ -6,6 +6,11 @@ register(
     timestep_limit=1000,
 )
 register(
+    id='fast-trading-v0',
+    entry_point='gym_trading.envs:FastTradingEnv',
+    timestep_limit=1000,
+)
+register(
     id='complex-trading-v0',
     entry_point='gym_trading.envs:MultiStockTradingEnv',
     timestep_limit=1000,
