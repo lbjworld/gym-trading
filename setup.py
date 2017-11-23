e90c7ea013734b7b9efa5d7895e3623f459906ef
lbjworld <lbj.world@gmail.com>
2017-11-23 17:19:22 +0800

    FIX: setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import find_packages, setup
 
 setup(name='gym_trading',
       version='0.0.3',
-      install_requires=['gym', 'pandas', 'pandas_datareader', 'git+https://github.com/lbjworld/tushare.git'],
+      install_requires=['gym', 'pandas', 'pandas_datareader'],
+      dependency_links=['git+https://github.com/lbjworld/tushare.git@master'],
       packages=find_packages(),
 )
